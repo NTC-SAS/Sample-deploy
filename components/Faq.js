b5b2f5a381b845fbf2a56f260e78ff10e11f6a50
NTC-SAS <NTC_SAS_2022@outlook.com>
2022-06-23 11:17:25 +0800

    Changing the text color of learn more

--- components/Faq.js
+++ components/Faq.js
@@ -29,7 +29,7 @@ export default function Faq() {
                           <h1 className="mb-3 text-lg font-medium text-gray-900 title-font">Office of the Student Affairs Office</h1>
                           <p className="mb-3 leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
                           <div className="flex flex-wrap items-center ">
-                            <a className="inline-flex items-center text-indigo-500 md:mb-2 lg:mb-0" href="./osa/osa.html">Learn More
+                            <a className="inline-flex items-center text-red-500 md:mb-2 lg:mb-0" href="./osa/osa.html">Learn More
                               <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
                                 <path d="M5 12h14"></path>
                                 <path d="M12 5l7 7-7 7"></path>
